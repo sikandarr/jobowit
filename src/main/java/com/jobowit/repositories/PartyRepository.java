52506ff8f21951b8cba85a42e352b72edf99bb6c
Sikandar <sikandar.b@mail.com>
2017-06-14 03:34:22 +0930

    included REST dependency Spring's auto imp of REST

--- src/main/java/com/jobowit/repositories/PartyRepository.java
+++ src/main/java/com/jobowit/repositories/PartyRepository.java
@@ -2,11 +2,10 @@ package com.jobowit.repositories;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import com.jobowit.domain.Party;
 
-@Repository
+@RepositoryRestResource
 public interface PartyRepository  extends JpaRepository<Party, Long>
 {
 	List<Party> findByContactName(String contactName);

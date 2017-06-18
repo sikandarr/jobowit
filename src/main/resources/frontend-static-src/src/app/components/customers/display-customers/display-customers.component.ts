7bdc120dc1a684c091ca4303ce3fcc967fb4ca36
Mearaj <mearajbhagad@gmail.com>
2017-06-18 16:12:24 +0530

    modification frontend sources file

---
+++ src/main/resources/frontend-static-src/src/app/components/customers/display-customers/display-customers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-customers',
+  templateUrl: './display-customers.component.html',
+  styleUrls: ['./display-customers.component.scss']
+})
+export class DisplayCustomersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/main/resources/frontend-static-src/src/app/models/customer.ts
@@ -0,0 +1,16 @@
+export default {
+  id: "ID",
+  name: "Name",
+  contactName: "Contact Name",
+  phone: "Phone",
+  mobile: "Mobile"
+}
+
+export class Customer {
+  id: string;
+  name: string;
+  contactName: string;
+  phone: number;
+  mobile: number;
+  email: string;
+}

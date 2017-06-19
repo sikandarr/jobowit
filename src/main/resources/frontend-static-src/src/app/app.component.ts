688e260ab2a1c12c78ab96c4bc49c1228a059fc4
Mearaj <mearajbhagad@gmail.com>
2017-06-19 11:06:11 +0530

    now only static contents from frontend side in static folder

--- src/main/resources/frontend-static-src/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import {ServerService} from "./services/ServerService";
-
-@Component({
-  // tslint:disable-next-line
-  selector: 'app-root',
-  templateUrl: './app.component.html'
-})
-export class AppComponent { }

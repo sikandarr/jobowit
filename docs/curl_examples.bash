90096f521fa22ab27bec9312c703db7881e1109d
Sikandar <sikandar.b@mail.com>
2017-06-30 15:15:45 +0930

    created bash file for curl command which inludes POST examples

---
+++ docs/curl_examples.bash
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+
+## POST new data with the child object inline/nested
+curl -v POST --data \
+'{
+	"name": "AJX Innovate Tech",
+	"contact_name": "Stephen Local",
+	"email": "stephen@local.host",
+	"mobile": "0450198231",
+	"phone": "0827578170",
+	"mailing_address": {
+		"street1": "42 Westwile Road",
+		"city": "Kalamata",
+		"state": "Victoria",
+		"post_code": "3055",
+		"country": "Australia"
+	},
+	"physical_address": {
+		"street1": "67/133 Anzac Hwy",
+		"city": "Kurralta Park",
+		"state": "South Australia",
+		"post_code": "5037",
+		"country": "Australia"
+	}
+}' "localhost:8080/api/parties/" --header "Content-Type:application/json"
+
+
+## POST new data with the child object assigned in a single property (single-String argument constructor)
+curl -v POST --data \
+'{
+	"name": "AJX Innovate Tech",
+	"contact_name": "Stephen Local",
+	"email": "stephen@local.host",
+	"mobile": "0450198231",
+	"phone": "0827578170",
+	"mailing_address": "24 Bradford Avenue:Bradford:New South Wales:4008:Australia",
+	"physical_address": "8 Grimsone Road:Flinder Park:New South Wales:4016:Australia"
+}' "localhost:8080/api/parties/" --header "Content-Type:application/json"
+

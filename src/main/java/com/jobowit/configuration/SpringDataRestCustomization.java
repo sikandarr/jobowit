15bec31f81dd37ad93b03d080675625a4f3ce202
Sikandar <sikandar.b@mail.com>
2017-07-18 03:07:47 +0930

    implemented uuid for Jobs

--- src/main/java/com/jobowit/configuration/SpringDataRestCustomization.java
+++ src/main/java/com/jobowit/configuration/SpringDataRestCustomization.java
@@ -4,7 +4,9 @@ import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import org.springframework.stereotype.Component;
 
+import com.jobowit.domain.Job;
 import com.jobowit.domain.Party;
+import com.jobowit.repositories.JobRepository;
 import com.jobowit.repositories.PartyRepository;
 
 @Component
@@ -15,6 +17,7 @@ public class SpringDataRestCustomization extends RepositoryRestConfigurerAdapter
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config)
 	{
 		config.withEntityLookup().forRepository(PartyRepository.class, Party::getUuid, PartyRepository::findByUuid);
+		config.withEntityLookup().forRepository(JobRepository.class, Job::getUuid, JobRepository::findByUuid);
 
 		config.getCorsRegistry().addMapping("/api/**").allowedOrigins("http://localhost:4200").allowedMethods("*")
 				.allowedHeaders("header1", "header2", "header3").exposedHeaders("header1", "header2")

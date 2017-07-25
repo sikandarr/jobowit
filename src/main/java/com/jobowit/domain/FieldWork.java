7f1d85b8caf07500eb7e9f96b85c948664b75aff
Sikandar <sikandar.b@mail.com>
2017-07-26 03:44:50 +0930

    included staff name

--- src/main/java/com/jobowit/domain/FieldWork.java
+++ src/main/java/com/jobowit/domain/FieldWork.java
@@ -5,81 +5,97 @@ import javax.persistence.*;
 
 import java.util.Date;
 
-
 /**
  * The persistent class for the field_work database table.
  * 
  */
 @Entity
-@Table(name="field_work")
-@NamedQuery(name="FieldWork.findAll", query="SELECT f FROM FieldWork f")
-public class FieldWork implements Serializable {
+@Table(name = "field_work")
+@NamedQuery(name = "FieldWork.findAll", query = "SELECT f FROM FieldWork f")
+public class FieldWork implements Serializable
+{
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="field_work_id", unique=true, nullable=false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "field_work_id", unique = true, nullable = false)
 	private int fieldWorkId;
 
 	@Temporal(TemporalType.TIMESTAMP)
-	@Column(name="finish_dtm", nullable=false)
+	@Column(name = "finish_dtm", nullable = false)
 	private Date finishDtm;
 
 	@Temporal(TemporalType.TIMESTAMP)
-	@Column(name="start_dtm", nullable=false)
+	@Column(name = "start_dtm", nullable = false)
 	private Date startDtm;
 
-	//bi-directional many-to-one association to Job
+	// bi-directional many-to-one association to Job
 	@ManyToOne
-	@JoinColumn(name="job_id", nullable=false)
+	@JoinColumn(name = "job_id", nullable = false)
 	private Job job;
 
-	//bi-directional many-to-one association to Staff
+	// bi-directional many-to-one association to Staff
 	@ManyToOne
-	@JoinColumn(name="field_staff_id", nullable=false)
+	@JoinColumn(name = "field_staff_id", nullable = false)
 	private Staff staff;
 
-	public FieldWork() {
+	public FieldWork()
+	{
 	}
 
-	public int getFieldWorkId() {
+	public int getFieldWorkId()
+	{
 		return this.fieldWorkId;
 	}
 
-	public void setFieldWorkId(int fieldWorkId) {
+	public void setFieldWorkId(int fieldWorkId)
+	{
 		this.fieldWorkId = fieldWorkId;
 	}
 
-	public Date getFinishDtm() {
+	public Date getFinishDtm()
+	{
 		return this.finishDtm;
 	}
 
-	public void setFinishDtm(Date finishDtm) {
+	public void setFinishDtm(Date finishDtm)
+	{
 		this.finishDtm = finishDtm;
 	}
 
-	public Date getStartDtm() {
+	public Date getStartDtm()
+	{
 		return this.startDtm;
 	}
 
-	public void setStartDtm(Date startDtm) {
+	public void setStartDtm(Date startDtm)
+	{
 		this.startDtm = startDtm;
 	}
 
-	public Job getJob() {
+	public Job getJob()
+	{
 		return this.job;
 	}
 
-	public void setJob(Job job) {
+	public void setJob(Job job)
+	{
 		this.job = job;
 	}
 
-	public Staff getStaff() {
+	public Staff getStaff()
+	{
 		return this.staff;
 	}
 
-	public void setStaff(Staff staff) {
+	public void setStaff(Staff staff)
+	{
 		this.staff = staff;
+	}
+	
+	public String getStaffName()
+	{
+		return this.getStaff().getName();
 	}
 
 }
